a5406b99bdf64d8c8e04952511e3fbdd8f36a92d
李德永 <lideyong@newcapec.net>
2025-09-11 09:17:31 +0800

    添加打包后的文件

---
+++ dist/detail.js
@@ -0,0 +1,70 @@
+
+const textures = {
+    oak: {
+        id: 'oak',
+        name: '橡木面板',
+        texture: {
+            map: '/textures/oak_veneer_01_diff_1k.jpg',
+        }
+    },
+    plywood: {
+        id: 'plywood',
+        name: '合成木板',
+        texture: {
+            map: '/textures/plywood_diff_1k.jpg',
+        }
+    },
+    rosewood: {
+        id: 'rosewood',
+        name: '红木面板',
+        texture: {
+            map: '/textures/rosewood_veneer1_diff_1k.jpg',
+        }
+    },
+    carbon: {
+        id: 'carbon',
+        name: '碳纤维面板',
+        texture: {
+            map: '/textures/carbon-fiber_metallic.png',
+        }
+    },
+    walnut: {
+        id: 'walnut',
+        name: '胡桃木面板',
+        texture: {
+            map: '/textures/walnut_diff_1k.jpg',
+        }
+    },
+    
+}
+
+export default textures;
+
+export const frameTypes = [
+    {
+        id: 'standard',
+        name: '标准支架',
+    },
+    {
+        id: 'adjustable',
+        name: '可调节支架',
+    },
+]
+
+export const frameColors = [
+    {
+        id: '#303030',
+        name: '黑色',
+    },
+    {
+        id: '#f0f0f0',
+        name: '白色',
+    },
+    {
+        id: '#a0a0a0',
+        name: '灰色',
+    },
+]
+
+
+
